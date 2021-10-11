c8ecd45aceb40153604c469a293eb3a5315c6c75
kristi0403 <kristirapi@hotmail.it>
2021-10-11 19:36:13 +0000

    Completed the database conections between each other

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Device, Special_category, Sub_category, Service
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Device)
+admin.site.register(Special_category)
+admin.site.register(Sub_category)
+admin.site.register(Service)
--- products/models.py
+++ products/models.py
@@ -12,15 +12,73 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class Special_category(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Sub_category(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Service(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Device(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
+    sub_category = models.ForeignKey(
+        'Sub_category', null=True, blank=True, on_delete=models.SET_NULL
+    ),
+    special_category = models.ForeignKey(
+        'Special_category', null=True, blank=True, on_delete=models.SET_NULL
+    ),
+    device = models.ForeignKey(
+        'Device', null=True, blank=True, on_delete=models.SET_NULL
+    ),
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2,
-                                 null=True, blank=True)
+    grade = models.TextField(max_length=1, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
