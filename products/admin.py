8fb5d1f7f5cd67668fdd819b8ac92acb6f5208e8
kristi0403 <kristirapi@hotmail.it>
2021-10-12 18:03:55 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,63 @@ from django.contrib import admin
 from .models import Product, Category, Device, Special_category, Sub_category, Service
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Device)
-admin.site.register(Special_category)
-admin.site.register(Sub_category)
-admin.site.register(Service)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'grade',
+        'image',
+        'device',
+        'special_category',
+        'sub_category',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'price',
+    )
+
+
+class Special_categoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class Sub_categoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(Special_category, Special_categoryAdmin)
+admin.site.register(Sub_category, Sub_categoryAdmin)
+admin.site.register(Service, ServiceAdmin)
